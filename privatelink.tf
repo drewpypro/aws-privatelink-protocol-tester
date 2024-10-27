76675d2252d41949e8152cbb424ec73027c7f5d2
drewpypro <83561525+drewpypro@users.noreply.github.com>
2024-10-26 21:53:54 -0700

    update privatelink

--- privatelink.tf
+++ privatelink.tf
@@ -25,6 +25,7 @@ resource "aws_vpc_endpoint_service" "producer_tcp_privatelink_service" {
 #   vpc_id                = aws_vpc.consumer_vpc.id
 #   service_name          = aws_vpc_endpoint_service.producer_udp_privatelink_service.service_name
 #   vpc_endpoint_type     = "Interface"
+#   subnet_ids            = [aws_subnet.consumer_endpoint_subnet.id]
 #   security_group_ids    = [aws_security_group.consumer_privatelink_sg.id]
 
 #   subnet_configuration {
