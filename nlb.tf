1cb2a320ade80012f73b90fc4da9c2e50276de05
drewpypro <83561525+drewpypro@users.noreply.github.com>
2024-10-27 14:34:48 -0700

    adding ssh testing

--- nlb.tf
+++ nlb.tf
@@ -84,6 +84,26 @@ resource "aws_lb_target_group" "tcp_tg_53" {
   }
 }
 
+resource "aws_lb_target_group" "tcp_tg_22" {
+  name        = "tcp-tg-22"
+  port        = 22
+  protocol    = "TCP"
+  vpc_id      = aws_vpc.producer_vpc.id
+  target_type = "instance"
+
+  health_check {
+    protocol            = "TCP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "tcp-tg-22"
+  }
+}
+
 # resource "aws_lb_listener" "udp_listener" {
 #   load_balancer_arn = aws_lb.udp_nlb.arn
 #   port              = 53
@@ -126,6 +146,21 @@ resource "aws_lb_listener" "tcp_53_listener" {
   }
 }
 
+resource "aws_lb_listener" "tcp_22_listener" {
+  load_balancer_arn = aws_lb.multi_tcp_nlb.arn
+  port              = 22
+  protocol          = "TCP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tcp_tg_22.arn
+  }
+
+  tags = {
+    Name = "tcp-listener-22"
+  }
+}
+
+
 # resource "aws_lb_target_group_attachment" "udp_target_group_attachment" {
 #   target_group_arn = aws_lb_target_group.udp_tg.arn
 #   target_id        = aws_instance.producer_ec2.id
@@ -133,14 +168,20 @@ resource "aws_lb_listener" "tcp_53_listener" {
 # }
 
 
-resource "aws_lb_target_group_attachment" "tcp_target_group_attachment" {
+resource "aws_lb_target_group_attachment" "tcp_8080_target_group_attachment" {
   target_group_arn = aws_lb_target_group.tcp_tg_8080.arn
   target_id        = aws_instance.producer_ec2.id
   port             = 8080
 }
 
-resource "aws_lb_target_group_attachment" "udp_target_group_attachment" {
+resource "aws_lb_target_group_attachment" "tcp_53_target_group_attachment" {
   target_group_arn = aws_lb_target_group.tcp_tg_53.arn
   target_id        = aws_instance.producer_ec2.id
   port             = 53
+}
+
+resource "aws_lb_target_group_attachment" "tcp_22_target_group_attachment" {
+  target_group_arn = aws_lb_target_group.tcp_tg_22.arn
+  target_id        = aws_instance.producer_ec2.id
+  port             = 22
 }
